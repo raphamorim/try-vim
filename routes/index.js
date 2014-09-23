546497aa3bfe3c12d2a627f8b2b35cf199400be8
Raphael Amorim <rapha850@gmail.com>
2014-09-23 01:22:39 -0300

    remove random colors from render in route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var ejs = require('ejs'),
 	levels = require('./../levels');
 
-
 exports.course = function(req, res){
 	var lvl = (Math.round(req.params.level) - 1);
 
@@ -12,14 +11,5 @@ exports.course = function(req, res){
 };
 
 exports.index = function(req, res){
-	var colors = ['blue', 'red', 'green', 'purple'].sort(function() {
-  		return .3 - Math.random();
-	});
-
-	res.render('home.ejs', {
-		first:  colors[0],
-		second: colors[1],
-		third:  colors[2],
-		fourth: colors[3],
-	});
+	res.render('home.ejs');
 };
